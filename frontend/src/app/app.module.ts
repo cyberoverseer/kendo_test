a8cf1ce48159348a9404d8d2568364f3f3458c27
ryanborromeocodev <ryanb@codev.com>
2018-06-20 12:18:13 +0800

    Added Signin Component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { UserService } from './shared/user.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -9,20 +10,24 @@ import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { SignInComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent
+    SignupComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
     GridModule,
     FormsModule,
     ButtonsModule,
-    InputsModule
+    InputsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -1,5 +1,8 @@
+import { UserService } from './../shared/user.service';
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { User } from '../shared/user.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -9,9 +12,32 @@ import { User } from '../shared/user.model';
 export class SignupComponent implements OnInit {
   user: User;
 
-  constructor() { }
+  constructor(private userService: UserService, private toastr: ToastrService) { }
 
   ngOnInit() {
+    this.resetForm();
   }
 
+  resetForm(form?: NgForm) {
+
+    if (form != null) {
+      form.reset();
+      this.user = {
+        UserName: '',
+        Password: ''
+      };
+    }
+  }
+
+  OnSubmit(form: NgForm) {
+    this.userService.registerUser(form.value)
+      .subscribe((data: any) => {
+        if (data.Succeeded === true) {
+          this.resetForm(form);
+          this.toastr.success('User registration successful');
+        } else {
+          this.toastr.error(data.Errors[0]);
+        }
+      });
+}
 }
