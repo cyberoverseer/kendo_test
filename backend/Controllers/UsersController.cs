557ab19497d973d8a3f3253e09714f5d69fc2aca
ryanborromeocodev <ryanb@codev.com>
2018-06-21 06:15:00 +0800

    Modified UsersController

--- backend/Controllers/UsersController.cs
+++ backend/Controllers/UsersController.cs
@@ -1,7 +1,38 @@
+using System.Threading.Tasks;
+using backend.Data;
+using backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
 namespace backend.Controllers
 {
-    public class UsersController
+    [Route("api/[controller]")]
+    public class UsersController : Controller
     {
-        
+        private readonly IAuthRepository _repo;
+        public UsersController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(string username, string password)
+        {
+            //Validate Request
+
+            username = username.ToLower();
+
+            if(await _repo.UserExists(username))
+                return BadRequest("Username is already taken");
+
+            var userToCreate = new User
+            {
+                Username = username
+            };
+
+            var createdUser = await _repo.Register(userToCreate, password);
+
+            return StatusCode(201); 
+        }
+
     }
 }
