8f4e1f0b8b8bb3c75769752f90734af6abd89af3
ryanborromeocodev <ryanb@codev.com>
2018-06-22 10:15:31 +0800

    Updated User Registration

--- backend/Controllers/UsersController.cs
+++ backend/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using backend.Data;
+using backend.Dtos;
 using backend.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,21 +16,22 @@ namespace backend.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register(string username, string password)
+        public async Task<IActionResult> Register([FromBody]UserForRegisterDto userForRegisterDto)
         {
             //Validate Request
 
-            username = username.ToLower();
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+            userForRegisterDto.Password = userForRegisterDto.Password.ToLower();
 
-            if(await _repo.UserExists(username))
+            if(await _repo.UserExists(userForRegisterDto.Username))
                 return BadRequest("Username is already taken");
 
             var userToCreate = new User
             {
-                Username = username
+                Username = userForRegisterDto.Username
             };
 
-            var createdUser = await _repo.Register(userToCreate, password);
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
             return StatusCode(201); 
         }
